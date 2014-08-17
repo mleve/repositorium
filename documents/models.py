a450ce3ee8f66fe733e27adb20c2b7f1f93a58df
Mario Leverone <mleve@ug.uchile.cl>
2014-08-17 13:31:41 -0400

    agregada restriccion unique a nombre de documentos

--- documents/models.py
+++ documents/models.py
@@ -2,10 +2,15 @@ from django.db import models
 from django.contrib.auth.models import User
 
 class Document(models.Model):
-	name = models.CharField(max_length=45)
+	name = models.CharField(max_length=45, unique=True)
 	description = models.TextField()
 	creator = models.ManyToManyField(User)
+	def __unicode__(self):
+		return self.name
+
+def get_document_name(instance,filename):
+	return "/".join([instance.document.name,filename])
 
 class File(models.Model):
 	document = models.ForeignKey(Document)
-	value = models.FileField( upload_to = "document_extras")
+	value = models.FileField( upload_to = get_document_name)
