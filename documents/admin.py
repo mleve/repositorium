9e3934c0b676fa5099192e06c7081f6b6c4a8d61
Mario Leverone <mleve@ug.uchile.cl>
2014-08-18 10:48:27 -0400

    agregado FullFill y payments

--- documents/admin.py
+++ documents/admin.py
@@ -1,5 +1,6 @@
 from django.contrib import admin
-from documents.models import Document,File
+from documents.models import Document,File,Payment
 # Register your models here.
 admin.site.register(Document)
 admin.site.register(File)
+admin.site.register(Payment)
--- documents/models.py
+++ documents/models.py
@@ -1,5 +1,6 @@
 from django.db import models
 from django.contrib.auth.models import User
+from core.models import Criterion
 
 class Document(models.Model):
 	name = models.CharField(max_length=45, unique=True)
@@ -14,3 +15,23 @@ def get_document_name(instance,filename):
 class File(models.Model):
 	document = models.ForeignKey(Document)
 	value = models.FileField( upload_to = get_document_name)
+	def __unicode__(self):
+		return self.value.name
+
+class Payment(models.Model):
+	user = models.ForeignKey(User)
+	document = models.ForeignKey(Document)
+	class Meta:
+		unique_together = (("user","document"),)
+	def __unicode__(self):
+		return self.user.username +" has paid for" + self.document.name
+
+class Fullfill(models.Model):
+	criterion = models.ForeignKey(Criterion)
+	document = models.ForeignKey(Document)
+	status = models.IntegerField()
+	positive = models.IntegerField()
+	negative = models.IntegerField()
+	validated_date = models.DateField()
+	class Meta:
+		unique_together = (("criterion","document"),)
