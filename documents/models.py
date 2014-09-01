04968965fa6156788797c6af4422462af1e5ed37
LalSoong <nataliandrea@ug.uchile.cl>
2014-09-01 11:48:46 -0400

    Crear Documento

--- documents/models.py
+++ documents/models.py
@@ -5,16 +5,16 @@ from core.models import Criterion
 class Document(models.Model):
 	name = models.CharField(max_length=45, unique=True)
 	description = models.TextField()
-	creator = models.ManyToManyField(User)
+	creator = models.ForeignKey(User)
 	def __unicode__(self):
 		return self.name
 
-def get_document_name(instance,filename):
+def get_file_name(instance,filename):
 	return "/".join([instance.document.name,filename])
 
 class File(models.Model):
 	document = models.ForeignKey(Document)
-	value = models.FileField( upload_to = get_document_name)
+	value = models.FileField( upload_to = get_file_name)
 	def __unicode__(self):
 		return self.value.name
 
--- documents/views.py
+++ documents/views.py
@@ -1,3 +1,13 @@
-from django.shortcuts import render
+from django.shortcuts import render_to_response
+from django.http import HttpResponse
+from django.contrib.auth import get_user_model
+from core.models import Document, File, Payment, Fullfill
+import json
 
 # Create your views here.
+
+def create_document(request):
+	response = {}
+	name = request.POST['name']
+	description = request.POST['description']
+	creator = request.POST['creator']
