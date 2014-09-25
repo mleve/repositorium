8cc4177a8d52fce110d131465cdd8563e1b8ab0e
Mario Leverone <mleve@ug.uchile.cl>
2014-09-25 15:26:50 -0300

    primer listado de apps hecho

--- console/views.py
+++ console/views.py
@@ -1,10 +1,14 @@
 from django.shortcuts import render_to_response
 from django.template import RequestContext
 from django.contrib.auth.decorators import login_required
+from core.models import App
 
 
 # Create your views here.
 
 @login_required()
 def home(request):
-	return render_to_response('list_apps.html', context_instance=RequestContext(request))
+	apps = App.objects.filter(developers = request.user)
+	return render_to_response('list_apps.html',
+		{'apps' : apps},
+		 context_instance=RequestContext(request))
